063ee282c5142aed558d11d738054973fcd75ed3
Anuj sahu <58728839+anuj326@users.noreply.github.com>
2023-07-30 12:07:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ const { adminLogin } = require('./controllers/adminLogin');
 const { volunteerData } = require('./controllers/volunteerData');
 const port = process.env.PORT || 8000;
 const app = express();
-app.use(cors({credential:true, }));
+//app.use(cors({credential:true, }));
+const corsConfig = {
+    origin: '',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}
+app.use(cors(corsConfig))
+app.options("", cors(corsConfig));
+
 app.use(express.json());
 
 app.get('/',function(req, res){
